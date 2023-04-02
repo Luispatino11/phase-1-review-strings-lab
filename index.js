9656c666de9812669ec7f525377b208a5b9c40be
luis patino <lcp.patino@gmail.com>
2023-04-02 09:44:15 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+
+let currentUser = 'John';
+
+
+console.log('The current user is:', currentUser);
+ 
+let welcomeMessage = 'Welcome to Flatbook, ';
+
+welcomeMessage += currentUser + '!'
+
+console.log(welcomeMessage);
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+console.log(excitedWelcomeMessage);
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+console.log(shortGreeting);
